58fb106d4ee16db9ff4e9ed0dd8267176099be3a
EmirhanTopuz <111640402+EmirhanTopuz@users.noreply.github.com>
2022-11-27 13:29:42 +0300

    pazar günü yapılanlar

--- DummyBazarEmirhan2/Migrations/Configuration.cs
+++ DummyBazarEmirhan2/Migrations/Configuration.cs
@@ -17,6 +17,9 @@
         {
             context.ManagersType.AddOrUpdate(m => m.ID, new Models.ManagerType() { ID = 1, Name = "Admin" });
             context.ManagersType.AddOrUpdate(m => m.ID, new Models.ManagerType() { ID = 2, Name = "Moderetör" });
+            context.Managers.AddOrUpdate(m => m.ID, new Models.Manager() { ID = 1, Name = "Emirhan",Surname="topuz",Mail="emirhan2809@gmail.com",ManagerType_ID=1,Password="123456789",
+                UserName="e.topuz",IsActive=true});
+
         }
     }
 }
