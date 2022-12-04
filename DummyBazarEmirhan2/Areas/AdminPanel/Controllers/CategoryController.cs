4db459703f4869bf2438453a944578988480d2ca
EmirhanTopuz <111640402+EmirhanTopuz@users.noreply.github.com>
2022-12-04 11:51:41 +0300

    2.pazar yapılanlar

--- DummyBazarEmirhan2/Areas/AdminPanel/Controllers/CategoryController.cs
+++ DummyBazarEmirhan2/Areas/AdminPanel/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -32,7 +33,46 @@ namespace DummyBazarEmirhan2.Areas.AdminPanel.Controllers
                 return RedirectToAction("Index");
             }
             return View(model);
+        }
 
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            Category c = db.Categories.Find(id);
+            return View(c);
+        }
+        [HttpPost]
+        public ActionResult Edit(Category model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Entry(model).State = EntityState.Modified;
+                    db.SaveChanges();
+                    ViewBag.Mesaj = "düzenleme işlemi başarılı";
+                }
+                catch 
+                {
+                    ViewBag.Mesaj = "kategori düzenlenirken bi hata oluştu";
+                  
+                }
+            }
+            return View(model);
+        }
+        public ActionResult Delete(int? id)
+        {
+            if(id!= null)
+            {
+                Category c = db.Categories.Find(id);
+                db.Categories.Remove(c);
+                db.SaveChanges();
+            }
+            return RedirectToAction("index");
         }
     }
 }
